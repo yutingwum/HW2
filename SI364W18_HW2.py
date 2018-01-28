11d6a3550d58f6fc36328328190acd3a09e69ca9
Yuting Wu <phillng0073@Yutings-MacBook-Pro.local>
2018-01-28 14:52:40 -0500

    finished part 1,2,and 3. Add new templates for album entry

--- SI364W18_HW2.py
+++ SI364W18_HW2.py
@@ -15,6 +15,8 @@ from flask import Flask, request, render_template, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, RadioField, ValidationError
 from wtforms.validators import Required
+import requests
+import json
 
 #####################
 ##### APP SETUP #####
@@ -27,8 +29,10 @@ app.config['SECRET_KEY'] = 'hardtoguessstring'
 ###### FORMS #######
 ####################
 
-
-
+class AlbumEntryForm(FlaskForm):
+	album_name = StringField('Enter the name of an album: ', validators=[Required()])
+	rating = RadioField('How much do you like this album? (1 low, 3 high)', choices=[('1','Low'),('2','Medium'),('3', 'High')], validators=[Required()])
+	submit = SubmitField('Submit')
 
 ####################
 ###### ROUTES ######
@@ -43,6 +47,55 @@ def hello_world():
 def hello_user(name):
     return '<h1>Hello {0}<h1>'.format(name)
 
+
+
+@app.route('/artistform')
+def artistForm():
+    return render_template('artistform.html')
+
+@app.route('/artistinfo', methods=['POST', 'GET'])
+def artistInfo():
+	if request.method == 'GET':
+		artist = request.args.get("artist", "")
+		url = "https://itunes.apple.com/search?term=" + artist
+		r = requests.get(url)
+		artist_json = json.loads(r.text)
+
+		return render_template('artist_info.html', objects=artist_json['results'])
+
+@app.route('/artistlinks')
+def artistLinks():
+    return render_template('artist_links.html')
+
+@app.route('/specific/song/<artist_name>')
+def specific_artist(artist_name):
+	url = "https://itunes.apple.com/search?term=" + artist_name
+	r = requests.get(url)
+	artist_json = json.loads(r.text)
+	return render_template('specific_artist.html', results=artist_json['results'])
+
+@app.route('/album_entry')
+def album_entry():
+	simpleForm = AlbumEntryForm()
+	return render_template('album_entry.html', form=simpleForm)
+
+
+@app.route('/album_result', methods = ['GET', 'POST'])
+def album_result():
+	form = AlbumEntryForm(request.form)
+	if request.method == 'POST' and form.validate_on_submit():
+		album_name = form.album_name.data
+		print(album_name)
+		rating = form.rating.data
+		print(rating)
+		url = "https://itunes.apple.com/search?term=" + album_name + "&entity=album"
+		r = requests.get(url)
+		album_data = json.loads(r.text)
+		album_data['search_term'] = album_name
+		album_data['rating'] = rating
+		print(album_data)
+
+		return render_template('album_data.html', data=album_data)
 
 if __name__ == '__main__':
     app.run(use_reloader=True,debug=True)
